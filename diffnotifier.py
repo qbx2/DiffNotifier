31f488fb64e7cae893ba812a71d0c98926178eff
SunYeop Lee <sunyeop97@gmail.com>
2016-03-11 03:37:05 +0900

    Initial Commit

---
+++ diffnotifier.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import http.client
+import urllib.parse
+import json
+import difflib
+import re
+
+with open('access_token', 'r') as f:
+	ACCESS_TOKEN = f.read().strip()
+
+with open('target_id', 'r') as f:
+	TARGET_ID = f.read().strip()
+
+with open('target_url', 'r') as f:
+	TARGET_URL = f.read().strip()
+
+assert len(ACCESS_TOKEN)
+assert len(TARGET_ID)
+assert len(TARGET_URL)
+
+try:
+	with open('latest_contents', 'r') as f:
+		LATEST_CONTENTS = f.read()
+except FileNotFoundError:
+	LATEST_CONTENTS = ''
+
+def fetch_url(url):
+	pr = urllib.parse.urlparse(url)
+	c = {'': http.client.HTTPConnection, 'http': http.client.HTTPConnection, 'https': http.client.HTTPSConnection}.get(pr.scheme.lower(), None)(pr.netloc)
+	c.request('GET', pr.path, pr.query)
+	return c.getresponse().read().decode()
+
+def publish(access_token, target_id, message='', link=''):
+	c = http.client.HTTPSConnection('graph.facebook.com')
+	c.request('POST', '/v2.5/{}/feed'.format(target_id), urllib.parse.urlencode({'access_token': access_token, 'message': message, 'link': link}))
+	return c.getresponse().read().decode()
+
+sanitize = lambda x: re.sub(r'(\s)+', r'\1', re.sub(r'<[^>]+>', '\n', x)).strip()
+
+new_contents = fetch_url(TARGET_URL)
+
+try:
+	if len(LATEST_CONTENTS):
+		# differ
+		diff = list(difflib.unified_diff(sanitize(LATEST_CONTENTS), sanitize(new_contents)))[2:]
+		pdiff = ''.join(map(lambda x:x[1:], filter(lambda x:x.startswith('+'),diff))).strip()
+		mdiff = ''.join(map(lambda x:x[1:], filter(lambda x:x.startswith('-'),diff))).strip()
+
+		if len(pdiff) or len(mdiff):
+			summary = 'added:\n{}\n\ndeleted:\n{}\n'.format(pdiff, mdiff)
+			print(summary)
+
+			# publish
+			publish(ACCESS_TOKEN, TARGET_ID, summary, TARGET_URL)
+except:
+	import traceback
+	traceback.print_exc()
+finally:
+	with open('latest_contents', 'w') as f:
+		f.write(new_contents)
